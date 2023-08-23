54c3f6eced9927b91239b6f77d83ff56861f4e64
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-08-23 20:37:36 +0100

    add fileds country , city to user model

--- chatapp/models/user.py
+++ chatapp/models/user.py
@@ -1,17 +1,16 @@
-from chatapp.models import Model
-
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
 from sqlalchemy import Integer, String
-
+from sqlalchemy.orm import Mapped, mapped_column
+from chatapp.models import Model
 
 class User(Model):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-
     firstname: Mapped[str] = mapped_column(String(32), nullable=False)
     lastname: Mapped[str] = mapped_column(String(32), nullable=False)
-    username: Mapped[str] = mapped_column(String(32), nullable=False)
-    email: Mapped[str] = mapped_column(String(64), nullable=False)
+    username: Mapped[str] = mapped_column(String(32), nullable=False, unique=True, index=True)
+    email: Mapped[str] = mapped_column(String(64), nullable=False, unique=True, index=True)
     password: Mapped[str] = mapped_column(String(64), nullable=False)
+    country: Mapped[str] = mapped_column(String(64))
+    city: Mapped[str] = mapped_column(String(64))
+
