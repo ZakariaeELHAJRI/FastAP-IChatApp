140ef851d6741a930a14006ac9138d0640a6aceb
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-08-26 13:02:30 +0100

    import crud operations

---
+++ chatapp/crud/__init__.py
@@ -0,0 +1,4 @@
+
+from chatapp.crud.user import create_user, get_user, get_users, update_user, delete_user
+from chatapp.crud.friendships import create_friendship, get_friendship, get_friendships , update_friendship, delete_friendship
+from chatapp.crud.message import create_message, get_message, get_messages , update_message, delete_message
