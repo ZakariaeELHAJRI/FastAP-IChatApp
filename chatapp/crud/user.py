1973a79ea843fa0ea4a54e975bf5abb28de90bc6
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-08-26 13:01:06 +0100

    create crud opertations for user

---
+++ chatapp/crud/user.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+# import user model
+from chatapp.models.user import User
+
+def create_user(db: Session, user_data: dict):
+    user = User(**user_data)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(User).offset(skip).limit(limit).all()
+
+def update_user(db: Session, user_id: int, user_data: dict):
+    user = get_user(db, user_id)
+    if user is None:
+        return None
+    for key, value in user_data.items():
+        setattr(user, key, value)
+    db.commit()
+    db.refresh(user)
+    return user
+
+def delete_user(db: Session, user_id: int):
+    user = get_user(db, user_id)
+    if user is None:
+        return False
+    db.delete(user)
+    db.commit()
+    return True
