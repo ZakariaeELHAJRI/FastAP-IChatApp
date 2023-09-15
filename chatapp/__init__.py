8e81534c533ac43a99b2976e8fb1e2b0409303fa
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-15 22:05:15 +0100

    allow create new msg if friendship status is accepted

--- chatapp/__init__.py
+++ chatapp/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI , Depends
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base , SessionLocal
 from sqlalchemy.orm import Session
 from chatapp.routers import user, message, friendships  , authentication # Import routers
+from chatapp.auth import auth # Import auth
 from dotenv import load_dotenv
 from chatapp.database import get_db
 from dependencies.auth import get_current_user 
@@ -10,10 +12,24 @@ load_dotenv()
 # app
 app = FastAPI()
 
+# Configure CORS settings
+origins = [
+ "http://localhost:3000",  # Add the URL of your Next.js frontend
+    "http://localhost:8000",  # Add the URL of your FastAPI server
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # You can specify specific HTTP methods (e.g., ["GET", "POST"])
+    allow_headers=["*"],  # You can specify specific HTTP headers
+)
 
 
 # Define routes for CRUD operations
 app.include_router(authentication.router, tags=["authentication"])
+app.include_router(auth.auth_router, prefix="/api", tags=["auth"])
 app.include_router(user.router, prefix="/api", tags=["users"])
 app.include_router(message.router, prefix="/api", tags=["messages"])
 app.include_router(friendships.router, prefix="/api", tags=["friendships"])
--- chatapp/auth/auth.py
+++ chatapp/auth/auth.py
@@ -3,18 +3,21 @@ from dependencies.auth import TokenData, get_current_user
 from fastapi.security import  OAuth2PasswordRequestForm
 from dependencies.auth import authenticate_user, create_access_token
 from dependencies.auth import User, Token
+from chatapp.database import get_db
+from sqlalchemy.orm import Session
 auth_router = APIRouter()
 
 @auth_router.post("/login", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(form_data.username, form_data.password)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends() , db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password , db)
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     access_token = create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
 
-@auth_router.get("/users/me", response_model=User)
+@auth_router.get("/users/me/")
 async def read_users_me(current_user: User = Depends(get_current_user)):
     return current_user
+
 
 # Path: chatapp/auth/auth.py
