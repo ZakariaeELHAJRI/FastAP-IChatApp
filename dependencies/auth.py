81bae8013bdd332a9d7dad403d8e400a5cf113e2
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-15 19:58:47 +0100

    create auth file to handel auth methods

---
+++ dependencies/auth.py
@@ -0,0 +1,85 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta
+from passlib.context import CryptContext
+from chatapp.database import get_db
+from sqlalchemy.orm import Session
+from jose import JWTError, jwt
+from pydantic import BaseModel
+from chatapp.models import User
+from dotenv import load_dotenv
+load_dotenv()
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str = None    
+
+ # Password hashing
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")   
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(username: str, db: Session):
+    user = db.query(User).filter(User.username == username).first()
+    return user
+
+def authenticate_user(username: str, password: str, db: Session = Depends(get_db)):
+    user = get_user(username, db)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme) , db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username , db=db)
+    if user is None:
+        raise credentials_exception
+    return user
+
+pwd = get_password_hash("test123")
+
+print(pwd)
+
+
+
+# Path: dependencies/auth.py
+
+
