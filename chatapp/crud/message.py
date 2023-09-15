80037feacca2ce3440d19d2640de9bbb695b2857
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-15 22:04:55 +0100

    allow create new msg if friendship status is accepted

--- chatapp/crud/message.py
+++ chatapp/crud/message.py
@@ -1,8 +1,19 @@
 from sqlalchemy.orm import Session
 from chatapp.models.message import Message
+from chatapp.models.friendships import Friendship
 
-def create_message(db: Session, message_data: dict):
-    new_message = Message(**message_data)
+
+def create_message(db: Session, message_data: dict, sender_id: int, receiver_id: int):
+    # Check if a valid friendship exists between sender and receiver
+    friendship = db.query(Friendship).filter(
+        (Friendship.user_id == sender_id) & (Friendship.friend_id == receiver_id) &
+        (Friendship.status == "accepted")
+    ).first()
+
+    if not friendship:
+        raise ValueError("There is no accepted friendship between the sender and receiver.")
+
+    new_message = Message(**message_data, user_id=sender_id)
     db.add(new_message)
     db.commit()
     db.refresh(new_message)
