8a492940597dc6547b94b44659b463f76904b47c
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-20 23:46:56 +0100

    add get_current_user_websocket to handel request comming from socket

--- dependencies/auth.py
+++ dependencies/auth.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException , Request , WebSocket
 from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta
 from passlib.context import CryptContext
@@ -12,9 +12,6 @@ load_dotenv()
 SECRET_KEY = "your-secret-key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-
 
 
 class Token(BaseModel):
@@ -57,6 +54,25 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+async def get_current_user_websocket(token: str, db: Session):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username , db=db)
+    if user is None:
+        raise credentials_exception
+    return user
 
 async def get_current_user(token: str = Depends(oauth2_scheme) , db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
